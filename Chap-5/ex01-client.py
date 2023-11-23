0501e1cf9b1b9d89868542201181111ead3dd454
F. Ozgur Catak <70791004+ocatak@users.noreply.github.com>
2023-11-23 19:23:08 +0100

    Add files via upload

---
+++ Chap-5/ex01-client.py
@@ -0,0 +1,41 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Thu Nov  2 11:08:04 2023
+
+@author: ozgur
+"""
+
+import flwr as fl
+import tensorflow as tf
+import numpy as np
+
+# Load model and data (MNIST)
+model = tf.keras.Sequential([
+    tf.keras.layers.Flatten(input_shape=(28, 28)),
+    tf.keras.layers.Dense(128, activation="relu"),
+    tf.keras.layers.Dropout(0.2),
+    tf.keras.layers.Dense(10),
+])
+
+model.compile(optimizer="rmsprop", loss=tf.keras.losses.SparseCategoricalCrossentropy(from_logits=True), metrics=["accuracy"])
+(x_train, y_train), (x_test, y_test) = tf.keras.datasets.mnist.load_data()
+
+# Define Flower client
+class MnistClient(fl.client.NumPyClient):
+    def get_parameters(self, config):
+        return model.get_weights()
+
+    def fit(self, parameters, config):
+        model.set_weights(parameters)
+        model.fit(x_train, y_train, epochs=10, batch_size=2000, verbose=0)  # You can adjust batch_size
+        return model.get_weights(), len(x_train), {}
+
+    def evaluate(self, parameters, config):
+        model.set_weights(parameters)
+        loss, accuracy = model.evaluate(x_test, y_test)
+        model.save("global-model.tf")
+        return loss, len(x_test), {"accuracy": accuracy}
+
+# Start Flower client
+fl.client.start_numpy_client(server_address="[::]:8080", client=MnistClient())
---
+++ Chap-5/ex01-server.py
@@ -0,0 +1,15 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Thu Nov  2 11:07:44 2023
+
+@author: ozgur
+"""
+
+import flwr as fl
+
+# Start Flower server
+fl.server.start_server(
+  server_address="0.0.0.0:8080",
+  config=fl.server.ServerConfig(num_rounds=10),
+)
